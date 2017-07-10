1d8882c541d1d5991654655a3ec07f9634a98994
banette-liszt <arisa_16n1100989@nnn.ed.jp>
2017-07-10 15:44:54 +0900

    text4_45

--- higher.js
+++ higher.js
@@ -1,16 +1,32 @@
-//高階関数
-function myHigherFunction (func) {
-    console.log('myHigherFunction');
+// //高階関数
+// function myHigherFunction (func) {
+//     console.log('myHigherFunction');
 
-return outsideFunction;
+// return outsideFunction;
 
+// }
+
+// //普通の関数
+// function outsideFunction (value) {
+//     console.log('value:' + value);
+// }
+
+// // let returnValue = myHigherFunction (); 
+// // returnValue('引数だよ！？？！？');
+// myHigherFunction ()('引数だよ！？');
+
+//高階関数arrayWalkを定義
+function arrayWalk(data,f) {
+    for (var key in data) {
+        f(data[key],key);
+    }
 }
 
-//普通の関数
-function outsideFunction (value) {
-    console.log('value:' + value);
+//配列を処理するためのユーザー定義関数
+function showElement(value,key) {
+    console.log(key + ':'+value);
 }
 
-// let returnValue = myHigherFunction (); 
-// returnValue('引数だよ！？？！？');
-myHigherFunction ()('引数だよ！？');
+var ary = [1,2,4,8,16];
+// arrayWalk(ary,showElement);
+ary.forEach(showElement);
